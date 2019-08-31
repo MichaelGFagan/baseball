17c8b1109683a1d425bfd557f839c3a7f8bedc37
MichaelGFagan <michael.g.fagan@gmail.com>
2019-08-31 14:07:02 -0500

    Hotfix career batting fact table

--- baseball-dbt/models/marts/players/fct_batting_career.sql
+++ baseball-dbt/models/marts/players/fct_batting_career.sql
@@ -8,23 +8,23 @@ select
     b.person_id,
     count(distinct b.year_id) as seasons,
     -- count(distinct )
-    sum(b.games),
-    sum(b.plate_appearances),
-    sum(b.at_bats),
-    sum(b.runs),
-    sum(b.hits),
-    sum(b.doubles),
-    sum(b.triples),
-    sum(b.home_runs),
-    sum(b.times_on_base),
-    sum(b.outs_made),
-    sum(b.extra_base_hits),
-    sum(b.total_bases),
-    sum(b.runs_batted_in),
-    sum(b.stolen_bases),
-    sum(b.caught_stealing),
-    sum(b.walks),
-    sum(b.strikeouts),
+    sum(b.games) as games,
+    sum(b.plate_appearances) as plate_appearances,
+    sum(b.at_bats) as at_bats,
+    sum(b.runs) as runs,
+    sum(b.hits) as hits,
+    sum(b.doubles) as doubles,
+    sum(b.triples) as triples,
+    sum(b.home_runs) as home_runs,
+    sum(b.times_on_base) as times_on_base,
+    sum(b.outs_made) as outs_made,
+    sum(b.extra_base_hits) as extra_base_hits,
+    sum(b.total_bases) as total_bases,
+    sum(b.runs_batted_in) as runs_batted_in,
+    sum(b.stolen_bases) as stolen_bases,
+    sum(b.caught_stealing) as caught_stealing,
+    sum(b.walks) as walks,
+    sum(b.strikeouts) as strikeouts,
     case
         when sum(b.at_bats) = 0 then round(0, 3)
         else round(sum(b.hits) / sum(b.at_bats), 3)
@@ -45,11 +45,11 @@ select
         when sum(b.at_bats) = 0 then 0
         else round(sum(b.total_bases) / sum(b.at_bats), 3)
     end as on_base_plus_slugging,
-    sum(b.intentional_walks),
-    sum(b.hit_by_pitch),
-    sum(b.sacrifice_hits),
-    sum(b.sacrifice_flies),
-    sum(b.ground_into_double_plays),
+    sum(b.intentional_walks) as intentional_walks,
+    sum(b.hit_by_pitch) as hit_by_pitch,
+    sum(b.sacrifice_hits) as sacrifice_hits,
+    sum(b.sacrifice_flies) as sacrifice_flies,
+    sum(b.ground_into_double_plays) as ground_into_double_plays,
     case
         when sum(b.plate_appearances) = 0 then round(0, 3)
         else round(sum(b.home_runs) / sum(b.plate_appearances), 3)
@@ -59,7 +59,7 @@ select
         else round(sum(b.extra_base_hits) / sum(b.plate_appearances), 3)
     end as extra_base_hit_rate,
     case
-        when when sum(b.plate_appearances) = 0 then round(0, 3)
+        when sum(b.plate_appearances) = 0 then round(0, 3)
         else round(sum(b.strikeouts) / sum(b.plate_appearances), 3)
     end as strikeout_rate,
     case
@@ -67,7 +67,7 @@ select
         else round(sum(b.walks) / sum(b.plate_appearances), 3)
     end as walk_rate,
     case
-        when when sum(b.strikeouts) = 0 then round(0, 2)
+        when sum(b.strikeouts) = 0 then round(0, 2)
         else round(sum(b.walks) / sum(b.strikeouts), 2)
     end as walk_to_strikeout_ratio,
     case
@@ -76,3 +76,5 @@ select
     end as strikeout_to_walk_ratio
 
 from batting as b
+
+group by 1
