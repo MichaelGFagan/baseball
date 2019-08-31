6cf40aa5a9409110a083508c9f6ca582ac13ef83
MichaelGFagan <michael.g.fagan@gmail.com>
2019-08-31 12:51:21 -0500

    Update batting fact tables to fix divide by 0

--- baseball-dbt/models/marts/players/fct_batting_season.sql
+++ baseball-dbt/models/marts/players/fct_batting_season.sql
@@ -27,20 +27,36 @@ select
     b.caught_stealing,
     b.walks,
     b.strikeouts,
-    round(b.hits / b.at_bats, 3) as batting_average,
-    round(b.times_on_base / b.on_base_denominator, 3) as on_base_percentage,
-    round(b.total_bases / b.at_bats, 3) as slugging_percentage,
-    round(b.times_on_base / b.on_base_denominator, 3) + round(b.total_bases / b.at_bats, 3) as on_base_plus_slugging,
+    case
+        when b.at_bats = 0 then round(0, 3)
+        else round(b.hits / b.at_bats, 3)
+    end as batting_average,
+    case
+        when b.on_base_denominator = 0 then round(0, 3)
+        else round(b.times_on_base / b.on_base_denominator, 3)
+    end as on_base_percentage,
+    case
+        when b.at_bats = 0 then round(0, 3)
+        else round(b.total_bases / b.at_bats, 3)
+    end as slugging_percentage,
+    case
+        when b.on_base_denominator = 0 then 0
+        else round(b.times_on_base / b.on_base_denominator, 3)
+    end +
+    case
+        when b.at_bats = 0 then 0
+        else round(b.total_bases / b.at_bats, 3)
+    end as on_base_plus_slugging,
     b.intentional_walks,
     b.hit_by_pitch,
     b.sacrifice_hits,
     b.sacrifice_flies,
-    b.ground_into_double_plays,
-    round(b.home_runs / b.plate_appearances, 3) as home_run_rate,
+    b.ground_into_double_plays
+    {# round(b.home_runs / b.plate_appearances, 3) as home_run_rate,
     round(b.extra_base_hits / b.plate_appearances, 3) as extra_base_hit_rate,
     round(b.strikeouts / b.plate_appearances, 3) as strikeout_rate,
     round(b.walks / b.plate_appearances, 3) as walk_rate,
     round(b.walks / b.strikeouts, 1) as walk_to_strikeout_ratio,
-    round(b.strikeouts / b.walks, 1) as strikeout_to_walk_ratio
+    round(b.strikeouts / b.walks, 1) as strikeout_to_walk_ratio #}
 
 from batting as b
