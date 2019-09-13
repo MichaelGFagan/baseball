8c8846c80a9efed5a009baedd6819ea2b55bbc46
MichaelGFagan <michael.g.fagan@gmail.com>
2019-09-12 21:53:49 -0500

    Add manager fact models

---
+++ baseball-dbt/models/marts/managers/fct_manager_career.sql
@@ -0,0 +1,32 @@
+with managers as (
+
+    select * from {{ ref('stg_lahman__managers') }}
+
+),
+
+teams as (
+
+    select * from {{ ref('stg_lahman__teams') }}
+
+),
+
+sums as (
+
+    select
+        m.person_id,
+        count(distinct m.year_id) as seasons,
+        min(m.year_id) as debut_year,
+        max(m.year_id) as final_year,
+        count(distinct t.franchise_id) count_franchises_managed,
+        sum(m.games) as games,
+        sum(m.wins) as wins,
+        sum(m.losses) as losses
+
+    from managers as m
+    join teams as t using (team_id)
+
+    group by 1
+
+)
+
+select * from sums
---
+++ baseball-dbt/models/marts/managers/fct_manager_season.sql
@@ -0,0 +1,19 @@
+with managers as (
+
+    select * from {{ ref('stg_lahman__managers') }}
+
+)
+
+select
+    m.person_id,
+    m.year_id,
+    m.team_id,
+    m.league_id,
+    m.order_in_season,
+    m.games,
+    m.wins,
+    m.losses,
+    m.position_in_standings,
+    m.is_player_manager
+
+from managers as m
