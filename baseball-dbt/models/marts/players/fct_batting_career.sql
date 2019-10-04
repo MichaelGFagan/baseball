acafd74047d5420411b2f094bacf36d5cc571d39
MichaelGFagan <michael.g.fagan@gmail.com>
2019-10-03 21:21:12 -0500

    Update batting and pitching career fact models

--- baseball-dbt/models/marts/players/fct_batting_career.sql
+++ baseball-dbt/models/marts/players/fct_batting_career.sql
@@ -4,37 +4,44 @@ with batting as (
 
 ),
 
+teams as (
+
+    select * from {{ ref('stg_lahman__teams') }}
+
+),
+
 sums as (
 
     select
         b.person_id,
         count(distinct b.year_id) as seasons,
-        count(distinct b.team_id) as teams,
-        sum(ifnull(b.games, 0)) as games,
-        sum(ifnull(b.plate_appearances, 0)) as plate_appearances,
-        sum(ifnull(b.at_bats, 0)) as at_bats,
-        sum(ifnull(b.runs, 0)) as runs,
-        sum(ifnull(b.hits, 0)) as hits,
-        sum(ifnull(b.doubles, 0)) as doubles,
-        sum(ifnull(b.triples, 0)) as triples,
-        sum(ifnull(b.home_runs, 0)) as home_runs,
-        sum(ifnull(b.times_on_base, 0)) as times_on_base,
-        sum(ifnull(b.outs_made, 0)) as outs_made,
-        sum(ifnull(b.extra_base_hits, 0)) as extra_base_hits,
-        sum(ifnull(b.total_bases, 0)) as total_bases,
-        sum(ifnull(b.runs_batted_in, 0)) as runs_batted_in,
-        sum(ifnull(b.stolen_bases, 0)) as stolen_bases,
-        sum(ifnull(b.caught_stealing, 0)) as caught_stealing,
-        sum(ifnull(b.walks, 0)) as walks,
-        sum(ifnull(b.strikeouts, 0)) as strikeouts,
-        sum(ifnull(b.intentional_walks, 0)) as intentional_walks,
-        sum(ifnull(b.hit_by_pitch, 0)) as hit_by_pitch,
-        sum(ifnull(b.sacrifice_hits, 0)) as sacrifice_hits,
-        sum(ifnull(b.sacrifice_flies, 0)) as sacrifice_flies,
-        sum(ifnull(b.ground_into_double_plays, 0)) as ground_into_double_plays,
-        sum(ifnull(b.on_base_denominator, 0)) as on_base_denominator
+        count(distinct t.franchise_id) as franchises,
+        {{ all_null_or_sum('b.games') }} as games,
+        {{ all_null_or_sum('b.plate_appearances') }} as plate_appearances,
+        {{ all_null_or_sum('b.at_bats') }} as at_bats,
+        {{ all_null_or_sum('b.runs') }} as runs,
+        {{ all_null_or_sum('b.hits') }} as hits,
+        {{ all_null_or_sum('b.doubles') }} as doubles,
+        {{ all_null_or_sum('b.triples') }} as triples,
+        {{ all_null_or_sum('b.home_runs') }} as home_runs,
+        {{ all_null_or_sum('b.times_on_base') }} as times_on_base,
+        {{ all_null_or_sum('b.outs_made') }} as outs_made,
+        {{ all_null_or_sum('b.extra_base_hits') }} as extra_base_hits,
+        {{ all_null_or_sum('b.total_bases') }} as total_bases,
+        {{ all_null_or_sum('b.runs_batted_in') }} as runs_batted_in,
+        {{ all_null_or_sum('b.stolen_bases') }} as stolen_bases,
+        {{ all_null_or_sum('b.caught_stealing') }} as caught_stealing,
+        {{ all_null_or_sum('b.walks') }} as walks,
+        {{ all_null_or_sum('b.strikeouts') }} as strikeouts,
+        {{ all_null_or_sum('b.intentional_walks') }} as intentional_walks,
+        {{ all_null_or_sum('b.hit_by_pitch') }} as hit_by_pitch,
+        {{ all_null_or_sum('b.sacrifice_hits') }} as sacrifice_hits,
+        {{ all_null_or_sum('b.sacrifice_flies') }} as sacrifice_flies,
+        {{ all_null_or_sum('b.ground_into_double_plays') }} as ground_into_double_plays,
+        {{ all_null_or_sum('b.on_base_denominator') }} as on_base_denominator
 
     from batting as b
+    join teams as t using (year_id, team_id)
 
     group by 1
 
@@ -43,7 +50,7 @@ sums as (
 select
     s.person_id,
     s.seasons,
-    s.teams,
+    s.franchises,
     s.games,
     s.plate_appearances,
     s.at_bats,
@@ -61,54 +68,21 @@ select
     s.caught_stealing,
     s.walks,
     s.strikeouts,
-    case
-        when s.at_bats = 0 then round(0, 3)
-        else round(s.hits / s.at_bats, 3)
-    end as batting_average,
-    case
-        when s.on_base_denominator = 0 then round(0, 3)
-        else round(s.times_on_base / s.on_base_denominator, 3)
-    end as on_base_percentage,
-    case
-        when s.at_bats = 0 then round(0, 3)
-        else round(s.total_bases / s.at_bats, 3)
-    end as slugging_percentage,
-    case
-        when s.on_base_denominator = 0 then 0
-        else round(s.times_on_base / s.on_base_denominator, 3)
-    end +
-    case
-        when s.at_bats = 0 then 0
-        else round(s.total_bases / s.at_bats, 3)
-    end as on_base_plus_slugging,
+    {{ no_divide_by_zero('s.hits', 's.at_bats', 3) }} as batting_average,
+    {{ no_divide_by_zero('s.times_on_base', 's.on_base_denominator', 3) }} as on_base_percentage,
+    {{ no_divide_by_zero('s.total_bases', 's.at_bats', 3) }} as slugging_percentage,
+    {{ no_divide_by_zero('s.times_on_base', 's.on_base_denominator', 3) }} + {{ no_divide_by_zero('s.total_bases', 's.at_bats', 3) }} as on_base_plus_slugging,
+    {# on_base_percentage + slugging_percentage as ops_test, #}
     s.intentional_walks,
     s.hit_by_pitch,
     s.sacrifice_hits,
     s.sacrifice_flies,
     s.ground_into_double_plays,
-    case
-        when s.plate_appearances = 0 then round(0, 3)
-        else round(s.home_runs / s.plate_appearances, 3)
-    end as home_run_rate,
-    case
-        when s.plate_appearances = 0 then round(0, 3)
-        else round(s.extra_base_hits / s.plate_appearances, 3)
-    end as extra_base_hit_rate,
-    case
-        when s.plate_appearances = 0 then round(0, 3)
-        else round(s.strikeouts / s.plate_appearances, 3)
-    end as strikeout_rate,
-    case
-        when s.plate_appearances = 0 then round(0, 3)
-        else round(s.walks / s.plate_appearances, 3)
-    end as walk_rate,
-    case
-        when s.strikeouts = 0 then round(0, 2)
-        else round(s.walks / s.strikeouts, 2)
-    end as walk_per_strikeout,
-    case
-        when s.walks = 0 then round(0, 2)
-        else round(s.strikeouts / s.walks, 2)
-    end as strikeout_per_walk
+    {{ no_divide_by_zero('s.home_runs', 's.plate_appearances', 3) }} as home_run_rate,
+    {{ no_divide_by_zero('s.extra_base_hits', 's.plate_appearances', 3) }} as extra_base_hit_rate,
+    {{ no_divide_by_zero('s.strikeouts', 's.plate_appearances', 3) }} as strikeout_rate,
+    {{ no_divide_by_zero('s.walks', 's.plate_appearances', 3) }} as walk_rate,
+    {{ no_divide_by_zero('s.walks', 's.strikeouts', 3) }} as walk_per_strikeout,
+    {{ no_divide_by_zero('s.strikeouts', 's.walks', 3) }} as strikeout_per_walk
 
 from sums as s
--- baseball-dbt/models/marts/players/fct_pitching_career.sql
+++ baseball-dbt/models/marts/players/fct_pitching_career.sql
@@ -4,37 +4,44 @@ with pitching as (
 
 ),
 
+teams as (
+
+    select * from {{ ref('stg_lahman__teams') }}
+
+),
+
 sums as (
 
     select
         p.person_id,
         count(distinct p.year_id) as seasons,
-        count(distinct p.team_id) as teams,
-        sum(ifnull(p.wins, 0)) as wins,
-        sum(ifnull(p.losses, 0)) as losses,
-        sum(ifnull(p.games, 0)) as games,
-        sum(ifnull(p.games_started, 0)) as games_started,
-        sum(ifnull(p.games_finished, 0)) as games_finished,
-        sum(ifnull(p.complete_games, 0)) as complete_games,
-        sum(ifnull(p.shutouts, 0)) as shutouts,
-        sum(ifnull(p.saves, 0)) as saves,
-        sum(ifnull(p.batters_faced, 0)) as batters_faced,
-        sum(ifnull(p.outs_pitched, 0)) as outs_pitched,
-        sum(ifnull(p.hits, 0)) as hits,
-        sum(ifnull(p.runs, 0)) as runs,
-        sum(ifnull(p.earned_runs, 0)) as earned_runs,
-        sum(ifnull(p.home_runs, 0)) as home_runs,
-        sum(ifnull(p.walks, 0)) as walks,
-        sum(ifnull(p.strikeouts, 0)) as strikeouts,
-        sum(ifnull(p.intentional_walks, 0)) as intentional_walks,
-        sum(ifnull(p.hit_by_pitch, 0)) as hit_by_pitch,
-        sum(ifnull(p.wild_pitches, 0)) as wild_pitches,
-        sum(ifnull(p.balks, 0)) as balks,
-        sum(ifnull(p.sacrifice_hits, 0)) as sacrifice_hits,
-        sum(ifnull(p.sacrifice_flies, 0)) as sacrifice_flies,
-        sum(ifnull(p.ground_into_double_plays, 0)) as ground_into_double_plays
+        count(distinct t.franchise_id) as franchises,
+        {{ all_null_or_sum('p.wins') }} as wins,
+        {{ all_null_or_sum('p.losses') }} as losses,
+        {{ all_null_or_sum('p.games') }} as games,
+        {{ all_null_or_sum('p.games_started') }} as games_started,
+        {{ all_null_or_sum('p.games_finished') }} as games_finished,
+        {{ all_null_or_sum('p.complete_games') }} as complete_games,
+        {{ all_null_or_sum('p.shutouts') }} as shutouts,
+        {{ all_null_or_sum('p.saves') }} as saves,
+        {{ all_null_or_sum('p.batters_faced') }} as batters_faced,
+        {{ all_null_or_sum('p.outs_pitched') }} as outs_pitched,
+        {{ all_null_or_sum('p.hits') }} as hits,
+        {{ all_null_or_sum('p.runs') }} as runs,
+        {{ all_null_or_sum('p.earned_runs') }} as earned_runs,
+        {{ all_null_or_sum('p.home_runs') }} as home_runs,
+        {{ all_null_or_sum('p.walks') }} as walks,
+        {{ all_null_or_sum('p.strikeouts') }} as strikeouts,
+        {{ all_null_or_sum('p.intentional_walks') }} as intentional_walks,
+        {{ all_null_or_sum('p.hit_by_pitch') }} as hit_by_pitch,
+        {{ all_null_or_sum('p.wild_pitches') }} as wild_pitches,
+        {{ all_null_or_sum('p.balks') }} as balks,
+        {{ all_null_or_sum('p.sacrifice_hits') }} as sacrifice_hits,
+        {{ all_null_or_sum('p.sacrifice_flies') }} as sacrifice_flies,
+        {{ all_null_or_sum('p.ground_into_double_plays') }} as ground_into_double_plays
 
     from pitching as p
+    join teams as t using (year_id, team_id)
 
     group by 1
 
@@ -43,7 +50,7 @@ sums as (
 select
     s.person_id,
     s.seasons,
-    s.teams,
+    s.franchises,
     s.wins,
     s.losses,
     s.games,
@@ -68,54 +75,18 @@ select
     s.sacrifice_hits,
     s.sacrifice_flies,
     s.ground_into_double_plays,
-    case
-        when (s.batters_faced - s.walks - s.hit_by_pitch - s.sacrifice_hits - s.sacrifice_flies) = 0 then 0
-        else round(s.hits / (s.batters_faced - s.walks - s.hit_by_pitch - s.sacrifice_hits - s.sacrifice_flies), 3)
-    end as batting_average_against,
-    case
-        when (s.batters_faced - s.walks - s.hit_by_pitch - s.sacrifice_hits - s.strikeouts - s.home_runs) = 0 then 0
-        else round((s.hits - s.home_runs) / (s.batters_faced - s.walks - s.hit_by_pitch - s.sacrifice_hits - s.strikeouts - s.home_runs), 3)
-    end as batting_average_on_balls_in_play,
-    case
-        when s.outs_pitched = 0 then 0
-        else round(s.runs / s.outs_pitched * 27, 2)
-    end as run_average,
-    case
-        when s.outs_pitched = 0 then 0
-        else round(s.earned_runs / s.outs_pitched * 27, 2)
-    end as earned_run_average,
+    {{ no_divide_by_zero('s.hits', 's.batters_faced - s.walks - s.hit_by_pitch - s.sacrifice_hits - s.sacrifice_flies', 3) }} as batting_average_against,
+    {{ no_divide_by_zero('s.hits - s.home_runs', 's.batters_faced - s.walks - s.hit_by_pitch - s.sacrifice_hits - s.strikeouts - s.home_runs', 3) }} as batting_average_on_balls_in_play,
+    {{ no_divide_by_zero('s.runs', 's.outs_pitched * 27', 2) }} as run_average,
+    {{ no_divide_by_zero('s.earned_runs', 's.outs_pitched * 27', 2) }} as earned_run_average,
     -- FIP: (13 * s.home_runs + 3 * (walks + hit_by_pitch) - 2 * s.strikeouts) / s.batters_faced * 27 + constant as fielding_independent_pitching,
-    case
-        when s.outs_pitched = 0 then 0
-        else round((s.walks + s.hits) * 3 / s.outs_pitched, 3)
-    end as whip,
-    case
-        when s.outs_pitched = 0 then 0
-        else round(s.hits / s.outs_pitched * 27, 1)
-    end as hits_per_nine,
-    case
-        when s.outs_pitched = 0 then 0
-        else round(s.home_runs / s.outs_pitched * 27, 1)
-    end as home_runs_per_nine,
-    case
-        when s.outs_pitched = 0 then 0
-        else round(s.walks / s.outs_pitched * 27, 1)
-    end as walks_per_nine,
-    case
-        when s.strikeouts = 0 then 0
-        else round(s.walks / s.strikeouts, 2)
-    end as walks_per_strikeout,
-    case
-        when s.batters_faced = 0 then 0
-        else round(s.strikeouts / s.batters_faced, 3)
-    end as strikeout_rate,
-    case
-        when s.batters_faced = 0 then 0
-        else round(s.walks / s.batters_faced, 3)
-    end as walk_rate,
-    case
-        when s.batters_faced = 0 then 0
-        else round(s.home_runs / s.batters_faced, 3)
-    end as home_run_rate
+    {{ no_divide_by_zero('s.walks + s.hits * 3', 's.outs_pitched', 3) }} as whip,
+    {{ no_divide_by_zero('s.hits', 's.outs_pitched * 27', 1) }} as hits_per_nine,
+    {{ no_divide_by_zero('s.home_runs', 's.outs_pitched * 27', 1) }} as home_runs_per_nine,
+    {{ no_divide_by_zero('s.walks', 's.outs_pitched * 27', 1) }} as walks_per_nine,
+    {{ no_divide_by_zero('s.walks', 's.strikeouts', 2) }} as walks_per_strikeout,
+    {{ no_divide_by_zero('s.strikeouts', 's.batters_faced', 3) }} as strikeout_rate,
+    {{ no_divide_by_zero('s.walks', 's.batters_faced', 3) }} as walk_rate,
+    {{ no_divide_by_zero('s.home_runs', 's.batters_faced', 3) }} as home_run_rate
 
 from sums as s
