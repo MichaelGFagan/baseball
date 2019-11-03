ec6c70803d9bf4e0c312424a51745125c5ae5e30
MichaelGFagan <michael.g.fagan@gmail.com>
2019-11-02 21:15:29 -0500

    Update dim_people models

--- baseball-dbt/models/marts/players/dim_people_players.sql
+++ baseball-dbt/models/marts/players/dim_people_players.sql
@@ -2,13 +2,38 @@ with people as (
 
     select * from {{ ref('stg_lahman__people') }}
 
+),
+
+register as (
+
+    select * from {{ ref('stg_chadwick__register') }}
+
 )
 
 select
+    r.person_id,
     p.baseball_reference_id,
     p.bats,
     p.throws,
     p.debut_game_date,
     p.final_game_date
+    r.first_pro_game_played,
+    r.last_pro_game_played,
+    r.first_mlb_game_played,
+    r.last_mlb_game_played,
+    r.first_college_game_played,
+    r.last_college_game_played,
+    r.first_pro_game_managed,
+    r.last_pro_game_managed,
+    r.first_mlb_game_managed,
+    r.last_mlb_game_managed,
+    r.first_college_game_managed,
+    r.last_college_game_managed,
+    r.first_pro_game_umpired,
+    r.last_pro_game_umpired,
+    r.first_mlb_game_umpired,
+    r.last_mlb_game_umpired
 
-from people as p
+from register as r
+left join people as p
+    on r.baseball_reference_id = p.baseball_reference_id
